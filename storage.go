3cdd25089a9f27243fdf9bd851abef343d381227
Kush238 <kushbrahmbhatt23@gmail.com>
2025-03-26 16:16:01 +0530

    Now it can display/load the todos from todos.json file and worked in:  main.go and  storage.go

--- storage.go
+++ storage.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Storage[T any] struct {
 	FileName string
 }
@@ -8,4 +13,26 @@ type Storage[T any] struct {
 
 func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
+}
+// Functions with first letter capital will only be exported to other files 
+func (s *Storage[T]) Save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "    ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(s.FileName, fileData, 0644)
+}
+
+func (s *Storage[T]) Load (data *T) error {
+	fileData, err := os.ReadFile(s.FileName)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(fileData, data) 
+	// Unmarshal will convert JSON data and convert it to data argument we pass
+
 }
